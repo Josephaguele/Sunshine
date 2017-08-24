f8b65b14c2b544b30ca1436bae84d94d79f1dc91
AGUELE OSEKUEMEN JOE <osekuemen@gmail.com>
2017-08-24 23:23:11 +0100

    first chapter commit

---
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -0,0 +1,107 @@
+package com.example.android.sunshine.app;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu, this add items to the action ar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar wil
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /*A placeholder fragment containing a simple view.
+     */
+
+    public static class PlaceholderFragment extends Fragment {
+
+        ArrayAdapter<String> mForecastAdapter;
+
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+
+
+            // Once the root view for the Fragment has been created, its is
+            // the ListView with some dummy data.
+
+            // Create some dummy data for the ListView. Here's a sample weekly
+            // that represents as per day, wheher, high/low
+
+            String[] sunshine = {"Today - Sunny - 88/63",
+                    "Tomorrow - Foggy - 70/45",
+                    "Weds- Cloudy - 72/63",
+                    "Thurs - Rainy -65/51",
+                    "Friday - Foggy -704",
+                    "Saturday - Sunny - 76/68"};
+
+            List<String> weekForecast = new ArrayList<String>(Arrays.asList(sunshine));
+
+            // Now that we have some dummy forecast data, create an ArrayAdapter.
+            // The ArrayAdapter will take data froma source ( like our dummy forecast) and
+            // use it to populate the ListView it's attached to.
+
+            // 33 and 34
+            /*Initializing an array adapter*/
+            // Context
+            // Id of list item layout
+            // id of text view
+            // list of data
+            mForecastAdapter = new ArrayAdapter<String>(
+                    getActivity(), // The current context (this activity)
+                    R.layout.list_item_forecast, // The name of the layout ID
+                    R.id.list_item_forecast_textview, // The ID of the textview to populate
+                    weekForecast);
+            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+            //35. Finding Views  findViewById
+            //Get a reference to the ListView, and attach this adapter to it.
+            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+            listView.setAdapter(mForecastAdapter);
+
+            return rootView;
+        }
+    }
+
+
+}
